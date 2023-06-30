74e8c042e220af74435979b132adf50974b4daa9
petertrai <petertrai@gmail.com>
2023-06-30 10:11:32 -0700

    getting basics of model

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,5 +1,11 @@
-function getRecipeById(recipe_id) {
-    return Promise.resolve(`awesome recipe with id ${recipe_id}`)
+const db = require('../../data/db-config')
+
+async function getRecipeById(recipe_id) {
+    const recipeRows = await db('recipes as r')
+    .rightJoin('steps as s', 's.recipe_id', 'r.recipe_id')
+    .where('r.recipe_id', recipe_id)
+
+    return recipeRows
 }
 
 module.exports = {
