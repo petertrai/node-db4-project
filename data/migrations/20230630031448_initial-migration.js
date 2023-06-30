cbee86e49ae8c21c15fc2f868a6d86538e34bb70
petertrai <petertrai@gmail.com>
2023-06-29 20:39:09 -0700

    wip

---
+++ data/migrations/20230630031448_initial-migration.js
@@ -0,0 +1,35 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+  await knex.schema
+  .createTable('recipes', table => {
+    table.increments('recipe_id')
+    table.string('recipe_name', 200).notNullable().unique()
+  })
+  .createTable('ingredients', table => {
+    table.increments('ingredient_id')
+    table.string('ingredient_name', 200).notNullable().unique()
+    table.string('ingredient_unit', 50)
+  })
+  .createTable('steps', table => {
+    table.increments('step_id')
+    table.string('step_description')
+  })
+  .createTable('step_ingredients', table => {
+    table.increments('step_ingredient_id')
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+  await knex.schema
+  .dropTableIfExists('step_ingredients')
+  .dropTableIfExists('steps')
+  .dropTableIfExists('ingredients')
+  .dropTableIfExists('recipes')
+};
--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 const sharedConfig = {
   client: "sqlite3",
   useNullAsDefault: true,
-  migration: { directory: "./data/migrations" },
+  migrations: { directory: "./data/migrations" },
   seeds: { directory: "./data/seeds" },
   pool: {
     afterCreate: (conn, done) => conn.run("PRAGMA foreign_keys = ON", done),
